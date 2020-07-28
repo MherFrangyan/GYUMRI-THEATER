e9c1f13d6a08521b1a308342e3a327230f782991
Avetik <Av_mkhitaryan@bk.ru>
2020-07-28 11:53:41 +0400

    new

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+  // $('.input-group').datepick({format: 'dd/mm/yyyy'}); 
+
+  $(window).scroll(function(){
+    $('.navbar').toggleClass('navbar_bg', $(this).scrollTop() > 200);
+});
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  nav: true,
+       navText: [ `<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
+  responsive:{
+      0:{
+          items:1,
+          nav:false,
+      },
+      700:{
+        items:2,
+          nav:false,
+      },
+      1000:{
+          items:3,
+          nav:true,
+      },
+      1200:{
+        items:4,
+        nav:true,
+        margin:0, 
+      }
+  }
+
+});
+
+
+
+  });
