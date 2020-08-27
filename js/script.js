c0ad59d59936a3ca6d5618718ba0ae91b5fb8a86
MherFrangyan <mherf@mail.ru>
2020-08-27 10:40:39 +0400

    added page

--- js/script.js
+++ js/script.js
@@ -37,6 +37,36 @@ $('.owl-carousel').owlCarousel({
 
 });
 
+// $('.presentation-carusel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   responsiveClass:true,
+//   nav: true,
+//        navText: [ `<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
+//   responsive:{
+//       0:{
+//           items:1,
+//           nav:false,
+//       },
+//       700:{
+//         items:2,
+//           nav:false,
+//       },
+//       1000:{
+//           items:3,
+//           nav:true,
+//       },
+//       1200:{
+//         items:4,
+//         nav:true,
+//         margin:0, 
+//       }
+//   }
+
+// });
+
+
+
 
 
   });
