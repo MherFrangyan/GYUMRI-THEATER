b420a28e90527c207616d2a18f903af52045a0f3
MherFrangyan <mherf@mail.ru>
2020-09-10 12:30:07 +0400

    chenged font-family

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $('#datepicker').datepicker({
 });
 
 
-$('.owl-carousel').owlCarousel({
+$('#current_performance').owlCarousel({
   loop:true,
   margin:10,
   responsiveClass:true,
@@ -37,35 +37,63 @@ $('.owl-carousel').owlCarousel({
 
 });
 
-// $('.presentation-carusel').owlCarousel({
-//   loop:true,
-//   margin:10,
-//   responsiveClass:true,
-//   nav: true,
-//        navText: [ `<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
-//   responsive:{
-//       0:{
-//           items:1,
-//           nav:false,
-//       },
-//       700:{
-//         items:2,
-//           nav:false,
-//       },
-//       1000:{
-//           items:3,
-//           nav:true,
-//       },
-//       1200:{
-//         items:4,
-//         nav:true,
-//         margin:0, 
-//       }
-//   }
+$('#performances-carusel').owlCarousel({
+  loop:true,
+  margin:0,
+  responsiveClass:true,
+  nav: true,
+  navText: [ `<i class="fas fa-chevron-left"></i>`, `<i class="fas fa-chevron-right"></i>`],
+  responsive:{
+      0:{
+          items:1,
+          nav:false,
+      },
+      700:{
+        items:2,
+          nav:false,
+      },
+      1000:{
+          items:3,
+          nav:true,
+      },
+      1200:{
+        items:5,
+        nav:true,
+        margin:0, 
+      }
+  }
 
-// });
+});
 
 
+$('#searchBtn').click(function() {
+  $('.search_input').toggleClass('d-block');
+});
+
+
+$('#headerCarousel').owlCarousel({
+  items:1,
+  nav:true,
+  lazyLoad:true,
+  loop:true,
+  margin:0,
+  
+});
+
+$('.popup_youtube').magnificPopup({
+  disableOn: 700,
+  type: 'iframe',
+  preloader: false,
+
+});
+
+$('.performances-carusel').magnificPopup({
+    type: 'image',
+    delegate:'a'
+  });
+
+ 
+  
 
 
 
